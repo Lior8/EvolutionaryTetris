c1f7533cd01134f355bbe858470fe01761c63caf
Lior <50285865+Lior8@users.noreply.github.com>
2021-06-13 01:39:08 +0300

    Human Tetris An easy interface for humans to play the environment

---
+++ tetris_env/human_tetris.py
@@ -0,0 +1,34 @@
+from tetris_env import tetris
+from tetris_env.piece_factory import PieceFactory
+
+
+class HumanTetris:
+    """
+    Let a human player plays the Tetris environment
+    """
+    def __init__(self, height, width):
+        """
+        :param height: Height of the board
+        :param width: Width of the board
+        """
+        self.width = width
+        self.board = tetris.create_board(height, width)
+        self.pf = PieceFactory(width)
+
+    def play(self):
+        """
+        Game loop
+        """
+        tetris.print_board(self.board)
+        while True:
+            # Insert data as "piece_number num_of_rotations offset from the left"
+            pid, rid, col_offset = map(int, input().split())
+            # Retrieves the piece in its relevant rotation
+            piece = self.pf.pieces[pid][rid % len(self.pf.pieces[pid])]
+            # If you go over the maximum offset, it will set you to the maximum
+            if col_offset + len(piece[-1]) > self.width:
+                col_offset = self.width - len(piece[-1])
+            if tetris.drop_piece(piece, col_offset, self.board):
+                print('GAME OVER')
+                break
+            tetris.print_board(self.board)
