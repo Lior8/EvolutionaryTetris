ae20d50618330524c4806a5bb67525ae10267ae4
Lior <50285865+Lior8@users.noreply.github.com>
2021-06-13 16:14:31 +0300

    One-step lookahead The bot now considers the next piece when deciding the best way to drop the current piece

--- bot/tetris_bot.py
+++ bot/tetris_bot.py
@@ -10,17 +10,19 @@ class TetrisBot:
     """
     A bot which plays tetris given the feature weights
     """
-    def __init__(self, height, width, genome):
+    def __init__(self, height, width, genome, lookahead=True):
         """
         :param height: The board's height
         :param width:  The board's width
         :param genome: The weights for the features
+        :param lookahead: Should the bot look at the next piece when considering the best move?
         """
         self.height = height
         self.width = width
         self.weights = genome
         self.pf = PieceFactory(width)
         self.genome_len = len(genome)
+        self.lookahead = lookahead
 
     def play_game(self, with_print=False):
         """
@@ -34,7 +36,10 @@ class TetrisBot:
         next_pid = random.randint(0, 6)
         while True:
             pieces_counter += 1
-            board, _ = self.find_best_move(board, curr_pid)
+            if self.lookahead:
+                board, _ = self.find_best_move_lookahead(board, curr_pid, next_pid)
+            else:
+                board, _ = self.find_best_move(board, curr_pid)
             if board is None:
                 if with_print:
                     print('GAME OVER')
@@ -45,6 +50,30 @@ class TetrisBot:
             curr_pid = next_pid
             next_pid = random.randint(0, 6)
         return pieces_counter
+
+    def find_best_move_lookahead(self, board, cpid, npid):
+        """
+        Calculates the best move while also considering the next piece
+        :param board: The game board
+        :param cpid: The current piece's ID
+        :param npid: The next piece's ID
+        :return: The board after dropping the current piece in the best position (it does not drop the next one)
+        """
+        roas = self.pf.get_rotations_and_offset_limit(cpid)
+        max_score = - math.inf
+        best_board = None
+        for (piece, offset_limit) in roas:
+            for col_offset in range(offset_limit + 1):
+                new_board = tetris.copy_board(board)
+                if not tetris.drop_piece(piece, col_offset, new_board):
+                    # For each way to drop the current piece, we also evaluate all the ways we can drop the next piece
+                    # after we dropped the current one. The score of the current drop is the best score of the drop
+                    # of the next piece
+                    _, score = self.find_best_move(new_board, npid)
+                    if score > max_score:
+                        max_score = score
+                        best_board = new_board
+        return best_board, max_score
 
     def find_best_move(self, board, pid):
         """
