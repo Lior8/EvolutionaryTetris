e077aa4f6d9a349ccd3e9a0d15d67a6fb294b6a6
Lior <50285865+Lior8@users.noreply.github.com>
2021-06-14 18:57:00 +0300

    Parallel Evolution Documentation

--- evolution/parallel_evolution.py
+++ evolution/parallel_evolution.py
@@ -5,19 +5,27 @@ from evolution.evolution import EvolutionEnv
 
 
 class ParallelEvolutionEnv(EvolutionEnv):
+    """
+    An evolution environment with utilizes multiprocessing
+    """
     def __init__(self, pop_size=100, generations=40, p_mutation=0.2, p_crossover=1, k_tournament=3, g_size=7,
                  init_low_lim=-100, init_high_lim=100, games_per_fitness=5, board_height=12, board_width=6,
                  with_logging=True, with_printing=True, num_cores=-1):
+        """
+        :param num_cores: The number of processes in the pool. If it is not set, it will be equal to the number of cores
+        """
         super().__init__(pop_size, generations, p_mutation, p_crossover, k_tournament, g_size, init_low_lim,
                          init_high_lim, games_per_fitness, board_height, board_width, with_logging, with_printing)
         self.num_cores = multiprocessing.cpu_count() if num_cores == -1 else num_cores
 
     def calc_pop_fitness(self):
+        # Runs games for all individuals and log them
         results = mp.Pool(processes=self.num_cores).map(self.calc_fitness, self.population)
         for i in range(self.pop_size):
             self.logger.debug(f'G: {self.population[i]}')
             for j in range(self.games_per_fitness):
                 self.logger.debug(f'T{j}: {results[i][j]}')
+        # Turn the game scores into fitnesses
         self.fitnesses = [sum(result) / self.games_per_fitness for result in results]
 
     def calc_fitness(self, g):
